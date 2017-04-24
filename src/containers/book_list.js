eb14fcbcf3232ee9f7f5f1e59ed88a5fdd59ffdb
Punkbooster <ugliar.arsen@gmail.com>
2017-04-24 15:57:43 +0200

    Add ActionCreator & action

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+// action creator
+import { selectBook } from '../actions/index'
+// take value from action creator and make sure it updates through all reducers
+import { bindActionCreators } from 'redux'
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className='list-group-item'>{book.title}</li>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className='list-group-item'>
+          {book.title}
+        </li>
       )
     })
   }
@@ -26,6 +35,14 @@ function mapStateToProps(state) {
   }
 }
 
+// Anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBook is called, the result should be passed to all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
 // smart component is a component which has connection to a redux state
 // connect takes a function and a component and produces a container(smart component)
-export default connect(mapStateToProps)(BookList)
+// Promote BookList from a component to a container - it needs to know aboti this new dispatch method, selectBook.
+// Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList)
